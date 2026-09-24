02d964dddd9366f806806b189e225ba2affc9c6a
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 09:17:39 +0300

    pirm ir antr uzd 100%

--- Antra uzd/main.cpp
+++ Antra uzd/main.cpp
@@ -1,3 +1,33 @@
-//
-// Created by s067332 on 9/24/2026.
-//
+#include <iomanip>
+#include <iostream>
+using namespace std;//
+int main() {
+
+    /*  Pirma uzd
+    int a;
+    cout << "Enter a taig skaic: "<< endl;
+    cin >> a;
+    while (a<=0){
+        cout << "sk yra netinkamas"<< endl;
+        cout << "Enter a taig skaic: "<< endl;
+        cin >> a;
+
+    }
+    cout << a<<" yra taigamas"<< endl;
+*/
+    double saving =100.0;
+    const double target = 500.0,mdeposit= 75.0;
+    int month=0;
+    while(saving < target) {
+        month++;
+        saving += mdeposit;
+        cout<<month<< "menuo"<< fixed<<setprecision(2)<<saving<<"EUR"<<endl;
+
+
+    }
+    cout << "tikslas pataiktas"<< month<<endl;
+
+    
+
+    return 0;
+}
