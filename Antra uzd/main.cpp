3e51f4d6c6b6515d11c8b13e945971424bb2f8d3
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 09:50:06 +0300

    Ketvirtas uzd

--- Antra uzd/main.cpp
+++ Antra uzd/main.cpp
@@ -29,6 +29,7 @@ int main() {
 
     */
     // 3uzd
+    /*
     string password;
     do {cout << "Enter your password min 8 simbols: "<< endl;
         cin >> password;
@@ -40,7 +41,75 @@ int main() {
     }
 while (password.length() < 8);
     cout << "password is korect" <<1 endl;
+*/
+    // 4 uzd
+ int balance =100;
+int choise;
+    do
+    {
+    cout<<"Saskaitos menu : "<< endl;
+        cout << "1.perziureti saskaitos likutis"<< endl;
+        cout << "2.papilditi saskaitos"<< endl;
+        cout << "3. atlikti  mokejimo" <<endl;
+        cout << "01. baigti program"<< endl;
+        cout  << "Pasirinkite funcijos " << endl;
+    cin >>  choise;
+    switch(choise) {
+        case 1:
+            cout<<"saskaitos likutis"<<endl;
+            break;
+            case 2: {
+                int amount;
+                cout << "Papildimo suma" << endl;
+                cin >>  amount;
+                if(amount > 0) {
+                    balance += amount;
+                    cout << "saskaita yra papildita" << endl;
 
 
+                }
+                else {
+                    cout << "netinkama suma"<< endl;
+                }
+            break;
+            }
+            case 3: {
+
+                int amount;
+                cout << "mokejimo suma" << endl;
+                cin >>  amount;
+                if(amount <= 0) {
+                    cout << "netinkama suma"<< endl;
+                }
+                else if (amount > balance) {
+                    cout << "nepakankama likutis saskaitoja"<< endl;
+
+                }
+                else {
+                    balance -= amount;
+                    cout << "mokejomasa atlktas"<< endl;
+                }
+            break;
+            }
+case 0:
+    cout << "program baigta" << endl;
+break;
+
+            default:
+            cout << "tokios operacijos nera" << endl;
+
+
+
+
+
+
+
+
+    }
+
+
+    }
+
+    while (choise !=0);
     return 0;
 }
