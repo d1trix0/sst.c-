5d0865238318aaeb6d0a0606a082b9c5ded729c4
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 11:14:46 +0300

    Nuo pirmo iki 5

--- Pirma uzd/main.cpp
+++ Pirma uzd/main.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
 using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
 
 
-   /* string varda , pavarda , marks_car,tip_car,grupe,kursas,studija;
+  /*  string varda , pavarda , marks_car,tip_car,grupe,kursas,studija;
     float years ;
     cout << "enter your Varda"<<endl;
     cin >> varda;
     cout << "enter your Pavarda"<<endl;
     cin >> pavarda;
-
-
-    double svoris = 99.77;
-
-
-    string name_clubs;
-cout <<"enter your faivorite football club " << endl;
-    cin >> name_clubs;
-
-
-double svorisKonv = static_cast<double>(svoris);
-
-
-
     cout << "enter how old are you"<<endl;
     cin >> years;
     cout << "enter your grupe"<<endl;
@@ -34,27 +19,94 @@ double svorisKonv = static_cast<double>(svoris);
     cin >> kursas;
     cout << "enter your studija"<<endl;
     cin >> studija;
-    cout << "enter your kursas"<<endl;
-    cin >> kursas;
 
-cout << name_clubs<< endl;
-cout << "Jusu Vardas: "<<varda<< endl<<"Jusu Pavarda : "<< pavarda<< endl<<"your age are :"<<years << endl;
-cout << "Jusu grupe : " << grupe << endl;
+
+    double svoris = 99.77;
+    string saviviviniskas,stadion;
+
+    string name_clubs;
+    float club_metus;
+
+
+    cout <<"enter your faivorite football club " << endl;
+    cin >> name_clubs;
+
+    cout << "Enter Klubu metus : "<<endl;
+        cin>>club_metus;
+
+    cout << "Enter Savivininkas : "<< endl;
+        cin>>saviviviniskas;
+
+    cout << "Enter Stadionas : "<<endl;
+        cin>>stadion;
+
+
+
+    double svorisKonv = static_cast<double>(svoris);
+
+
+
+
+
+
+    cout << "Jusu Vardas: "<<varda<< endl<<"Jusu Pavarda : "<< pavarda<< endl<<"your age are :"<<years << endl;
+    cout << "Jusu grupe : " << grupe << endl;
     cout << "Jusu Kursas: " << kursas << endl;
     cout << "Jusu studija : " << studija << endl;
+    cout << "_________________________________________________________________________"<< endl;
+    cout << "Your Faivorite football club : "<<name_clubs<< endl;
+    cout << "Klubu metus : "<<club_metus<< endl;
+    cout << "Savivininkas : "<<saviviviniskas<< endl;
+    cout << "Stadionas : "<<stadion<< endl;
 */
+    //uzd 3
+    /*
+    string marke, modelis, spalva;
+    int metai;
+    double litrazas;
 
+    cout << "Iveskite automobilio marke: ";
+    cin >> marke;
 
-    int a;
-    cout << "Enter a taig skaic: "<< endl;
-    cin >> a;
-    while (a<=0){
-        cout << "sk yra netinkamas"<< endl;
-        cout << "Enter a taig skaic: "<< endl;
-        cin >> a;
+    cout << "Iveskite automobilio modeli: ";
+    cin >> modelis;
 
-    }
-    cout << a<<" yra taigamas"<< endl;
+    cout << "Iveskite pagaminimo metus: ";
+    cin >> metai;
+
+    cout << "Iveskite variklio litrąza: ";
+    cin >> litrazas;
+
+    cout << "Iveskite automobilio spalva: ";
+    cin >> spalva;
+
+    cout << "\nAutomobilis " << marke << " " << modelis
+         << " yra pagamintas " << metai << " metais. "
+         << "Jo motoras " << litrazas << " litro. "
+         << "Automobilis yra " << spalva << " spalvos." << endl;
+*/
+    //uzd 4
+    /*
+    int a=13,b=5;
+    float c =17.5,abc;
+
+abc=a + b - c ;
+    cout << abc << endl;
+    cout << 15 / 2 + c << endl;
+    cout << a / static_cast<double>(b) + 2 * c << endl;
+    cout << 14 % 3 + 6.3 + b / a << endl;
+    cout << static_cast<int>(c) % 5 + a - b << endl;
+    cout << 13.5 / 2 + 4.0 * 3.5 + 18 << endl;
+*/
+    //5 uzd
+    /*
+    cout << "enter 5 sk"<< endl;
+    int a1,a2,a3,a4,a5;
+    cin >> a1 >> a2 >> a3 >> a4 >> a5;
+    float Averagee=(a1+a2+a3+a4+a5)/5;
+    cout << "Average : " << Averagee << endl;
+*/
+    //6 uzd
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
