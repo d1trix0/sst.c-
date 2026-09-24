baf60d2664c5527952164c6a9b5c62156fa0aac9
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 11:17:48 +0300

    Sastas uzd

--- Pirma uzd/main.cpp
+++ Pirma uzd/main.cpp
@@ -107,6 +107,15 @@ abc=a + b - c ;
     cout << "Average : " << Averagee << endl;
 */
     //6 uzd
+    int sk;
+    float dvi,vien;
+    cout << "Enter dvizenklis sk : "<< endl;
+    cin>> sk;
+    dvi=sk/10;
+    vien= sk%10;
+    double sum;
+    sum=dvi+vien;
+    cout << sum << endl;
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
