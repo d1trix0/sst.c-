51a209e58973e93ce163902feb2c6a3950cce93c
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 11:30:12 +0300

    Pirma uzd

--- Trecia uzd/main.cpp
+++ Trecia uzd/main.cpp
@@ -3,9 +3,22 @@ using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-
+//uzd 1
     
+   cout  << "enter 5 sk"<< endl;
+    int a1,a2,a3,a4,a5;
+    cin >> a1 >> a2 >> a3 >> a4 >> a5;
+    double Average;
+    Average = (a1+a2+a3+a4+a5)/5;
+cout  << "average = " << Average<<endl;
+    if (Average < 5) {
+        cout << "tavo pazimis ne geras" << endl;
+    }
+    else {
+        cout << "tavo pazimis geras" << endl;
+    }
+
+
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
