71d33caafdad58319b847c65eb9f697976d3b397
Artem Zolotarov <s067332@ad.viko.lt>
2026-09-24 11:40:20 +0300

    Antras uzd

--- Trecia uzd/main.cpp
+++ Trecia uzd/main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-//uzd 1
-    
+//uzd 1 ir antras
+    /*
    cout  << "enter 5 sk"<< endl;
     int a1,a2,a3,a4,a5;
     cin >> a1 >> a2 >> a3 >> a4 >> a5;
@@ -16,8 +16,20 @@ cout  << "average = " << Average<<endl;
     }
     else {
         cout << "tavo pazimis geras" << endl;
-    }
-
+    }*/
+    //2 uzd;
+  /*
+    cout << "Entre your score : "<< endl;
+    int score;
+    cin >> score;
+    if (score ==10) {cout<< "puiku"<< endl;}
+    else if (score ==9) {cout<< "labai gerai"<< endl;}
+    else if (score >=7 &&score<9) {cout<< "normal"<< endl;}
+    else if (score<7 &&score >=4){cout<<"tu islalai egza"<<endl;}
+else if (score <4){cout << "Tu ne islakai egza"<< endl;}
+    else {cout<< "Tokios pazimes nera"<< endl;}
+*/
+//3 uzd
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
